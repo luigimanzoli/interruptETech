c966718b49d9b428aabfa9f62be303e6e93094f1
luigimanzoli <96997088+luigimanzoli@users.noreply.github.com>
2025-02-01 18:37:29 -0300

    Implementação do recurso de mudar a cor dos LEDs junto à menores otimizações no código.

--- interrupt.c
+++ interrupt.c
@@ -25,8 +25,20 @@
 // Definição dos botões
 #define BTNA_PIN 5
 #define BTNB_PIN 6
+#define BTNJ_PIN 22 // Botão do Joystick
 
-int contador = 0;
+// counter para mudança dos números da matriz
+int counter = 0;
+
+// counter para mudança das cores
+int count_color = 0;
+
+// Valores para definição das cores da matriz
+static volatile double r = 1.0; 
+static volatile double g = 1.0; 
+static volatile double b = 1.0;
+
+// Variável ligada ao debounce dos botões
 static volatile uint32_t last_time = 0; 
 
 void init_all() {
@@ -49,100 +61,13 @@ void init_all() {
     gpio_init(BTNB_PIN);
     gpio_set_dir(BTNB_PIN, GPIO_IN);
     gpio_pull_up(BTNB_PIN);
+
+    gpio_init(BTNJ_PIN);
+    gpio_set_dir(BTNJ_PIN, GPIO_IN);
+    gpio_pull_up(BTNJ_PIN);
 }
-
-void get_led(bool R, bool G, bool B) {
-    gpio_put(RLED_PIN, R);
-    gpio_put(GLED_PIN, G);
-    gpio_put(BLED_PIN, B);
-}
-
-/*
-
-double digit0[25] = {
-    0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0
-};
-
-double digit1[25] = {
-    0.0, 0.0, 1.0, 0.0, 0.0,
-    0.0, 1.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0
-};
-
-double digit2[25] = {
-    0.0, 0.0, 1.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 1.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0
-};
-
-double digit3[25] = {
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0
-};
-
-double digit4[25] = {
-    0.0, 1.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0, 0.0
-};
-
-double digit5[25] = {
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0
-};
-
-double digit6[25] = {
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 1.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0
-};
-
-double digit7[25] = {
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0, 0.0
-};
-
-double digit8[25] = {
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 1.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 1.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0
-};
-
-double digit9[25] = {
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 1.0, 0.0, 1.0, 0.0,
-    0.0, 1.0, 1.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0, 0.0
-};
-
-*/
 
 // Matriz com todos os dígitos
-
 double digits[10][25] = {
 { // Digito 0
     0.0, 1.0, 1.0, 1.0, 0.0,
@@ -217,50 +142,6 @@ double digits[10][25] = {
 
 };
 
-
-//todos apagados
-double desenho_apagado[25] = {
-    0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0
-};
-
-//ativando todas as cores
-double luz_total[25] = {
-    1.0, 1.0, 1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0, 1.0, 1.0
-};
-//ativando todas as cores com 80% de intensidade
-double luz_80_total[25] = {
-    0.8, 0.8, 0.8, 0.8, 0.8,
-    0.8, 0.8, 0.8, 0.8, 0.8,
-    0.8, 0.8, 0.8, 0.8, 0.8,
-    0.8, 0.8, 0.8, 0.8, 0.8,
-    0.8, 0.8, 0.8, 0.8, 0.8
-};
-//ativando todas as cores com 50% de intensidade
-double luz_50_total[25] = {
-    0.5, 0.5, 0.5, 0.5, 0.5,
-    0.5, 0.5, 0.5, 0.5, 0.5,
-    0.5, 0.5, 0.5, 0.5, 0.5,
-    0.5, 0.5, 0.5, 0.5, 0.5,
-    0.5, 0.5, 0.5, 0.5, 0.5
-};
-
-//ativando todas as cores com 20% de intensidade
-double luz_20_total[25] = {
-    0.2, 0.2, 0.2, 0.2, 0.2,
-    0.2, 0.2, 0.2, 0.2, 0.2,
-    0.2, 0.2, 0.2, 0.2, 0.2,
-    0.2, 0.2, 0.2, 0.2, 0.2,
-    0.2, 0.2, 0.2, 0.2, 0.2
-};
-
 //rotina para definição da intensidade de cores do led
 uint32_t matrix_rgb(double b, double r, double g)
 {
@@ -271,108 +152,106 @@ uint32_t matrix_rgb(double b, double r, double g)
   return (G << 24) | (R << 16) | (B << 8);
 }
 
-//rotina para acionar a matrix de leds - ws2812b
-void desenho_pio(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
-    for (int16_t i = 0; i < NUM_PIXELS; i++) {
-        // Define a cor vermelha para cada LED
-        valor_led = matrix_rgb(0.0, desenho[24 - i], 0.0); // Apenas o valor vermelho está ativo
-        pio_sm_put_blocking(pio, sm, valor_led); // Envia o valor para o LED
-    }
-
-}
-void desenho_apagado_total(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
-    for (int16_t i = 0; i < NUM_PIXELS; i++) {
-        // Define a cor nenhuma
-        valor_led = matrix_rgb(0.0, 0.0, 0.0); // Nenhum ativo
-        pio_sm_put_blocking(pio, sm, valor_led); // Envia o valor para o LED
-    }
-}
-//desenhando o azul completo
-void desenho_azul(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
-    for (int16_t i = 0; i < NUM_PIXELS; i++) {
-        // Define a cor azul para cada LED
-        valor_led = matrix_rgb(desenho[24 - i], 0.0, 0.0); // Apenas o valor azul está ativo
-        pio_sm_put_blocking(pio, sm, valor_led); // Envia o valor para o LED
-    }
-}
-
-void desenho_verde(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
-    for (int16_t i = 0; i < NUM_PIXELS; i++) {
-        // Define a cor azul para cada LED
-        valor_led = matrix_rgb(0.0, 0.0, desenho[24 - i]); // Apenas o valor verde está ativo
-        pio_sm_put_blocking(pio, sm, valor_led); // Envia o valor para o LED
-    }
-}
-void desenho_branco(double *desenho, uint32_t valor_led, PIO pio, uint sm, double r, double g, double b){
-
-    for (int16_t i = 0; i < NUM_PIXELS; i++) {
-        // Define a cor azul para cada LED
-        valor_led = matrix_rgb(desenho[24 - i], desenho[24 - i], desenho[24 - i]); // Todos ativos
-        pio_sm_put_blocking(pio, sm, valor_led); // Envia o valor para o LED
-    }
-}
 // Inicializa o sistema de clock
-void inicializar_clock() {
+void clock_init() {
     bool ok = set_sys_clock_khz(100000, false);
     if (ok) {
-        printf("Clock set to %ld\n", clock_get_hz(clk_sys));
+        printf("Clock setado para %ld Hz\n", clock_get_hz(clk_sys));
     } else {
         printf("Falha ao configurar o clock\n");
     }
 }
 
 // Configura a PIO
-void configurar_pio(PIO pio, uint *offset, uint *sm) {
+void pio_config(PIO pio, uint *offset, uint *sm) {
     *offset = pio_add_program(pio, &interrupt_program);
     *sm = pio_claim_unused_sm(pio, true);
     interrupt_program_init(pio, *sm, *offset, OUT_PIN);
 }
 
-void print_digit(int digit, PIO pio, uint sm){
+void print_digit(int digit, PIO pio, uint sm, double r, double g, double b){
+    // Valor para intensidade dos LEDs
+    int ity = 1;
 
-    uint32_t valor_led;
+    // Iniciando a variável que detém informação das cores de cada LED da matriz
+    uint32_t led_value;
+
+    // Condição para que os valores não ultrapassem o intervalor desejado
     if (digit <= 9 && digit >= 0){
         for (int16_t i = 0; i < NUM_PIXELS; i++) {
-            // Define a cor vermelha para cada LED
-            valor_led = matrix_rgb(0.0, 0.1*(digits[digit][24 - i]), 0.0); // Apenas o valor vermelho está ativo
-            pio_sm_put_blocking(pio, sm, valor_led); // Envia o valor para o LED
+
+            // Define a cor dos LEDs baseados nos valores de r, g e b
+            led_value = matrix_rgb(b*ity*(digits[digit][24 - i]), r*ity*(digits[digit][24 - i]), g*ity*(digits[digit][24 - i]));
+            pio_sm_put_blocking(pio, sm, led_value); // Envia o valor para o LED
+
         }
     } else if (digit < 0) {
         printf("Valor incompatível.\n");
-        contador = 0;
+        counter = 0;
     } else if (digit > 9){
         printf("Valor incompatível.\n");
-        contador = 9;
+        counter = 9;
     }
 }
 
+// Inicialização do PIO e das variáveis necessárias
 PIO pio = pio0;
-uint32_t valor_led;
+uint32_t led_value;
 uint offset, sm;
 
+// Função que é chamada quando ocorre a interrupção
 void gpio_irq_handler(uint gpio, uint32_t events){
 
+    // Definição da variável do tempo atual do sistema dês do começo
     uint32_t current_time = to_us_since_boot(get_absolute_time());
         if (current_time - last_time > 200000){
             last_time = current_time;
             
+            // Se o botão A for ativado, acrescenta na contagem
             if (gpio == BTNA_PIN){
 
-            contador++;
-            print_digit(contador, pio, sm);
-            printf("Contador = %i\n", contador);
+                counter++;
+                print_digit(counter, pio, sm, r, g, b);
+                printf("counter = %i\n", counter);
 
-            }   
+            }
+            // Se o botão B for ativado, descresce na contagem   
             else if (gpio == BTNB_PIN){
 
-            contador = contador - 1;
-            print_digit(contador, pio, sm);
-            printf("Contador = %i\n", contador);
+                counter = counter - 1;
+                print_digit(counter, pio, sm, r, g, b);
+                printf("counter = %i\n", counter);
 
+            } 
+            // Se o botão do Joystick for ativado, a cor dos números da contagem muda
+            else if (gpio == BTNJ_PIN){
+                count_color++;
+                printf("Mudança de cor. Count_color = %i\n", count_color);
+                if (count_color > 3){
+                    count_color = 0;
+                    r = 1.0;
+                    g = 1.0;
+                    b = 1.0;
+                    print_digit(counter, pio, sm, r, g, b);
+                }
+                else if (count_color == 1){
+                    r = 1.0;
+                    g = 0.0;
+                    b = 0.0;
+                    print_digit(counter, pio, sm, r, g, b);
+                }
+                else if (count_color == 2){
+                    r = 0.0;
+                    g = 1.0;
+                    b = 0.0;
+                    print_digit(counter, pio, sm, r, g, b);
+                }
+                else if (count_color == 3){
+                    r = 0.0;
+                    g = 0.0;
+                    b = 1.0;
+                    print_digit(counter, pio, sm, r, g, b);
+                }
             }
     }
 
@@ -383,20 +262,21 @@ int main() {
     // Inicializa clock, stdio e configurações
     stdio_init_all();
     init_all();
-    inicializar_clock();
+    clock_init();
 
     double r = 0.0, b = 0.0, g = 0.0;
 
-    configurar_pio(pio, &offset, &sm);
+    pio_config(pio, &offset, &sm);
 
-    printf("Sistema inicializado. Aguardando entrada...\n");
+    printf("Sistema inicializado.\n");
 
     // Configuração dos botões como interrupções
     gpio_set_irq_enabled_with_callback(BTNA_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(BTNB_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BTNJ_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     // Iniciando o sistema escrevendo 0 na matriz de LEDs
-    print_digit(0, pio, sm);
+    print_digit(0, pio, sm, 1, 1, 1);
 
     // Rotina inicial do programa para teste
     gpio_put(RLED_PIN, 1);
@@ -406,12 +286,12 @@ int main() {
     while (true) {
         /*
         if (!gpio_get(BTNA_PIN)){
-            contador++;
-            print_digit(contador, pio, sm);
+            counter++;
+            print_digit(counter, pio, sm);
         }
         else if (!gpio_get(BTNB_PIN)){
-            contador--;
-            print_digit(contador, pio, sm);
+            counter--;
+            print_digit(counter, pio, sm);
         }
         */
        // Repetição para que o LED vermelho pisque 5 vezes por segundo.
